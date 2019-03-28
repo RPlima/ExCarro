617683ec3ab888107ede19598e68fbafa5986fab
Raphael Lima Santana <raphaeltwins@hotmail.com>
2019-03-28 11:10:48 -0300

    Criando as ações no Main

--- ExCarro/Program.cs
+++ ExCarro/Program.cs
@@ -6,7 +6,32 @@ namespace ExCarro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IAutomovel automovel = new Carro();
+            automovel.AcenderFarol();
+            IAutomovel automovelB = new Carro();
+            IAutomovel automovelC = new Carro();
+            IAutomovel automovelD = new Carro();
+            IAutomovel automovelE = new Carro();
+            IAutomovel automovelF = new Carro();
+
+            Frotas_Carros grupoA = new Frotas_Carros();
+            grupoA.Automoveis.Add(automovel);
+            grupoA.Automoveis.Add(automovelB);
+            Frotas_Carros grupoB = new Frotas_Carros();
+            grupoB.Automoveis.Add(automovelC);
+            grupoB.Automoveis.Add(automovelD);
+            Frotas_Carros grupoGrande = new Frotas_Carros();
+            grupoGrande.Automoveis.Add(grupoA);
+            grupoGrande.Automoveis.Add(grupoB);
+            grupoGrande.Automoveis.Add(automovelE);
+            grupoGrande.Automoveis.Add(automovelF);
+
+            grupoA.AcenderFarol();
+            grupoB.ApagarFarol();
+
+            grupoGrande.Acelerar();
+
+            Console.ReadKey();
         }
     }
 }
